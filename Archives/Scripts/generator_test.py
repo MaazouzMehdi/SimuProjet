26f4a1092a840e9d72777f2932ef77b2b016ffff
elmimille6 <mimille6@gmail.com>
2018-05-26 19:14:45 +0200

    gaptest

--- Archives/Scripts/generator_test.py
+++ Archives/Scripts/generator_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import math
 import logging
-from scipy.stats import chi2
+from scipy.stats import chi2,kstest,chisquare,ksone
 import matplotlib.pyplot as plt
 
 def calculate_histo(list):
@@ -41,7 +41,67 @@ def testchi2(value, alpha=0.05):
         den=math.sqrt(tot/n)
         chi+=math.pow(dif/den,2)
     critical = chi2.ppf(1-alpha,n-1)
-    result= chi <critical
-
+    result = chi < critical
+    logging.debug("test chi2: chi= " + str(chi) + " critical= " + str(critical) + " result= " + str(result))
     return result
+
+def testchi2expected(value, expected, alpha=0.05):
+
+    n=(len(value))
+    chi=0
+    tot=0
+    for i in range(n):
+        tot+=value[i]
+    for i in range(n):
+        diff=value[i]-expected[i]
+        chi+=math.pow(diff,2)*1.0/expected[i]
+    critical = chi2.ppf(1-alpha,n-1)
+    result = chi < critical
+    logging.debug("test chi2: chi= " + str(chi) + " critical= " + str(critical) + " result= " + str(result))
+    return result
+
+# def kolmotest(value,alpha=0.05):
+#     x=kstest(value,"norm")
+#     print(x)
+#     (ks,pvalue)=x
+#     result=pvalue>alpha
+#     logging.debug("test kolmogorov-smirnov: ks= " + str(ks) + " pvalue= " + str(pvalue) + " result= " + str(result))
+#     logging.debug(len(value))
+
+
+
+# def testchi22(value,expectedvalue,alpha=0.05):
+#     x=chisquare(value,expectedvalue,ddof=alpha)
+#     print(x)
+#     # critical = chi2.ppf(1-alpha, (len(value)) - 1)
+#     (chi,pvalue)=x
+    # print(pvalue)
+    # result = chi < critical
+    # logging.debug("test chi22: chi= " + str(chi) + " critical= " + str(critical) + " result= " + str(result)+" pvalue= "+str(pvalue))
+    # return result
+
+def gapTest(value, a=0.0,b=0.5,t=10,alpha=0.05):
+    p=b-a
+    length=len(value)
+    len_sequence=[0]*t
+    n_gap=0
+    #calculate the gap
+    curr_distance=0
+    for i in range(length):
+        if not(a<=value[i] and value[i]<b):
+            curr_distance+=1
+            if i==length-1:
+                n_gap+=1
+        elif i!=0:
+            n_gap+=1
+            if curr_distance>=t:
+                curr_distance=t-1
+            len_sequence[curr_distance]+=1
+            curr_distance=0
+    #calculate a sequence expected for the chi2 test
+    len_expec_sequ=[0]*t
+    for i in range(t-1):
+        len_expec_sequ[i]=n_gap*p*math.pow(1-p,i)
+    len_expec_sequ[t-1]=n_gap*math.pow(1-p,t-1)
+    return testchi2expected(len_sequence,len_expec_sequ,alpha)
 
--- Archives/Scripts/main.py
+++ Archives/Scripts/main.py
@@ -14,32 +14,38 @@ logger = logging.getLogger()
 
 myGen=randomGenerator.PseudoRandomGenerator()
 # print(myGen.random())
-# list=myGen.getRandomList(1000)
+list=myGen.getRandomList(1000)
+gapTest(list)
+# test=[4,11,9,5,8,13,17,13,15,5]
+# kolmotest(test)
 # histo=calculate_histo(list)
 # print(list)
 # print(histo)
-# #print(testchi2(histo,[100]*10,0.05))
-res1=[0,0]
-res2=[0,0]
-for i in range(50):
-    print(i)
-    myGen = randomGenerator.PseudoRandomGenerator()
-    list = myGen.getRandomList(100)
-    histo = calculate_histo(list)
-    # print(histo)
-    chi=testchi2(histo)
-    if chi:
-        res1[0]+=1
-    else:
-        res1[1]+=1
-    listpy=randomGenerator.getPythonRandomList(100)
-    histopy = calculate_histo(listpy)
-    # print(histopy)
-    chi = testchi2(histopy)
-    if chi:
-        res2[0] += 1
-    else:
-        res2[1] += 1
-
-print(res1)
-print(res2)
+# test=[4,11,9,5,8,13,17,13,15,5]
+# testchi2(histo)
+# testchi22(histo)
+# testchi22(test,0.02)
+# res1=[0,0]
+# res2=[0,0]
+# for i in range(50):
+#     print(i)
+#     myGen = randomGenerator.PseudoRandomGenerator()
+#     list = myGen.getRandomList(100)
+#     histo = calculate_histo(list)
+#     # print(histo)
+#     chi=testchi2(histo)
+#     if chi:
+#         res1[0]+=1
+#     else:
+#         res1[1]+=1
+#     listpy=randomGenerator.getPythonRandomList(100)
+#     histopy = calculate_histo(listpy)
+#     # print(histopy)
+#     chi = testchi2(histopy)
+#     if chi:
+#         res2[0] += 1
+#     else:
+#         res2[1] += 1
+#
+# print(res1)
+# print(res2)
